25857c5a7a78f5025958664d17b06ed7a62b47f2
Alexandra Man <alexandra.man>
2022-06-16 15:10:04 +0300

    Created authentication methods -created login form -created register form -created logout

---
+++ petShop/src/Form/RegistrationFormType.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Positive;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', TextType::class, [
+                'constraints' => [
+                    new Email([
+                        'message' => "Make sure your email address is valid (contains '@', '.').",
+                    ]),
+                    new NotBlank([
+                        'message' => "Please fill out this field.",
+                    ]),
+                ],
+            ])
+            ->add('firstName', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Please fill out this field.",
+                    ]),
+                ],
+            ])
+            ->add('lastName', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Please fill out this field.",
+                    ]),
+                ],
+            ])
+            ->add('phoneNumber', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Please fill out this field.",
+                    ]),
+                    new Positive([
+                        'message' => "Please make sure it is a valid phone number.",
+                    ]),
+                    new Length([
+                        'min' => 10,
+                        'max' => 10,
+                    ])
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password.',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters.',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
